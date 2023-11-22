7d9bec2fce4b1fa6840b18c32b0d687e84150929
bunleaps <bunleap.sorn@gmail.com>
2023-11-22 21:09:35 +0700

    setup

--- db/connection.js
+++ db/connection.js
@@ -1,34 +1,38 @@
 /// grab environment variables
-require("dotenv").config()
+require("dotenv").config();
 /// IMPORT MONGOOSE
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 // IMPORT MERCED LOGGER FOR COLORFUL LOGS
-const {log} = require("mercedlogger")
+const { log } = require("mercedlogger");
 // Bring in our database string from .env or default string
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/defaultdb"
+const MONGODB_URL = process.env.MONGODB_URL;
 
 ///////////////////////////////////
 // Mongoose Configuration Object to Avoid Warnings
 ///////////////////////////////////
-const config = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
 
 ///////////////////////////////////
 // Making the Database Connection
 ///////////////////////////////////
-mongoose.connect(MONGODB_URL, config)
+mongoose.connect(MONGODB_URL, config);
 
 ///////////////////////////////////
 // Handling Connection Events
 ///////////////////////////////////
 mongoose.connection
-// Event for When Connection Opens
-.on("open", () => log.green("STATUS", "Connected to Mongo"))
-// Event for When Connection Closes
-.on("close", () => log.red("STATUS", "Disconnected from Mongo"))
-// Event for Connection Errors
-.on("error", (error) => log.red("ERROR", error))
+  // Event for When Connection Opens
+  .on("open", () => log.green("STATUS", "Connected to Mongo"))
+  // Event for When Connection Closes
+  .on("close", () => log.red("STATUS", "Disconnected from Mongo"))
+  // Event for Connection Errors
+  .on("error", (error) => log.red("ERROR", error));
 
 ///////////////////////////////////
 // Exporting Our Connection
 ///////////////////////////////////
-module.exports = mongoose
+module.exports = mongoose;
--- readme.md
+++ readme.md
@@ -1,13 +1,1 @@
-# BOILERPLATE DIRECTIONS
-
-- clone with command `npx degit githubusername/githubreponame#branchname projectName`
-
-- cd into new project folder
-
-- run `npm install` to install dependencies
-
-- rename template.env to .env
-
-- make sure to replace MONGODB_URL with a working Mongo URL
-
-- enjoy!
+# Collab Connect
